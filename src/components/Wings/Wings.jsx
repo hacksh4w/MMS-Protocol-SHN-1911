3fbe1b1004141dc995fc64eb14e7aff830315b2f
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-20 20:41:45 +0530

    tried fixing position for mobile devices part 7

--- src/components/Wings/Wings.jsx
+++ src/components/Wings/Wings.jsx
@@ -13,11 +13,11 @@ function Wings3(){
             }
         }
         else{
-            myMesh.current.rotation.z -= 0.025;
+            // myMesh.current.rotation.z -= 0.025;
         }
       });
     return(
-        <mesh ref={myMesh} position={hover ? [0.02,1.062,0] : [0.02,1.072,0]} onPointerOver={() => {setHover(true);}} onPointerOut={() => {setHover(false);}}>
+        <mesh ref={myMesh} position={hover ? [0.02,1.052,0] : [0.02,1.072,0]} onPointerOver={() => {setHover(true);}} onPointerOut={() => {setHover(false);}}>
             <mesh rotation={[0,0,0.82]} position={[-1.3,1.24,0]}>
                 <coneGeometry attatch="geometry" args={[0.2,3.5,64,1,0,6.283185307179586]}/>
                 <meshNormalMaterial attach="material"/>
