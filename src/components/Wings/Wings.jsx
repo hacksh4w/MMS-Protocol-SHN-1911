0e8f92fef2b6b308173d05d0bfef8544c29d98c0
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-20 20:08:21 +0530

    minor size changes part 3

--- src/components/Wings/Wings.jsx
+++ src/components/Wings/Wings.jsx
@@ -11,16 +11,16 @@ function Wings3(){
         }
       });
     return(
-        <mesh ref={myMesh} position={[0.02,1.06,0]} onPointerOver={() => {setHover(true);}} onPointerOut={() => {setHover(false);}}>
-            <mesh rotation={[0,0,0.8]} position={[-1.3,1.27,0]}>
+        <mesh ref={myMesh} position={[0.02,1.072,0]} onPointerOver={() => {setHover(true);}} onPointerOut={() => {setHover(false);}}>
+            <mesh rotation={[0,0,0.82]} position={[-1.3,1.24,0]}>
                 <coneGeometry attatch="geometry" args={[0.2,3.5,64,1,0,6.283185307179586]}/>
                 <meshNormalMaterial attach="material"/>
             </mesh>
-            <mesh rotation={[0,0,3.14159]} position={[0,-1.75,0]}>
+            <mesh rotation={[0,0,3.14159]} position={[0,-1.72,0]}>
                 <coneGeometry attatch="geometry" args={[0.2,3.5,64,1,0,6.283185307179586]}/>
                 <meshNormalMaterial attach="material"/>
             </mesh>
-            <mesh rotation={[0,0,-0.8]} position={[1.3,1.27,0]}>
+            <mesh rotation={[0,0,-0.82]} position={[1.3,1.24,0]}>
                 <coneGeometry attatch="geometry" args={[0.2,3.5,64,1,0,6.283185307179586]}/>
                 <meshNormalMaterial attach="material"/>
             </mesh>
