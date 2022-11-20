346793c4ce61167a825760885f51e6fc6624557b
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-20 17:26:12 +0530

    edited links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <div className='footer' style={{position: 'fixed',justifyContent: 'space-around'}}>
         <div className='container'>
             <ul>
-                <li><a href="https://www.energy.gov/clean-energy">Clean Energy </a></li>
-                <li><a href="https://www.millenniumfellows.org/meet-the-fellows">MF '22' </a></li>
-                <li><a href="https://www.worldwildlife.org/">WWF </a></li>
+                <li><a target='_blank' href="https://www.energy.gov/clean-energy">Clean Energy </a></li>
+                <li><a target='_blank' href="https://www.millenniumfellows.org/meet-the-fellows">MF '22' </a></li>
+                <li><a target='_blank' href="https://www.worldwildlife.org/">WWF </a></li>
             </ul>
         </div>
     </div>
